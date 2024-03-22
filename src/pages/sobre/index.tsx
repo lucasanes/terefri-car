de2d7c36077dd3c8d410865e1b6a08ef2fb75c1e
SkyLuke11 <sky.lucas003@gmail.com>
2024-03-22 20:21:38 -0300

    feat: add automatic tabs in about-us page

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -25,44 +25,39 @@ export function Sobre() {
     <S.Container size={60}>
       <S.Body>
         <S.Text>
-          {db.map((value, i) => (
-            <React.Fragment key={i}>
-              {value}
-              <br />
-              <br />
-            </React.Fragment>
-          ))}
+          {db.map((value, i) =>
+            i == 3 ? (
+              <React.Fragment key={i}>
+                <b>{value}</b>
+                <br />
+                <br />
+              </React.Fragment>
+            ) : (
+              i > 3 && (
+                <React.Fragment key={i}>
+                  {value}
+                  <br />
+                  <br />
+                </React.Fragment>
+              )
+            )
+          )}
         </S.Text>
         <br />
         <Tabs variant="bordered" size="md" className="tabs">
           <Tab key="vendas" title="Vendas à Vista">
             <Card>
-              <CardBody>
-                Vendas à vista: Oferecemos descontos especiais para pagamentos à
-                vista, proporcionando uma economia ainda maior na compra do seu
-                carro novo.
-              </CardBody>
+              <CardBody>Vendas à vista: {db[0]}</CardBody>
             </Card>
           </Tab>
           <Tab key="financiamento" title="Financiamento">
             <Card>
-              <CardBody>
-                Financiamento: Trabalhamos em parceria com instituições
-                financeiras renomadas para oferecer opções de financiamento com
-                taxas competitivas e condições flexíveis de pagamento. Nossa
-                equipe está preparada para auxiliá-lo em todo o processo, desde
-                a análise de crédito até a aprovação do financiamento.
-              </CardBody>
+              <CardBody>Financiamento: {db[1]}</CardBody>
             </Card>
           </Tab>
           <Tab key="trocas" title="Trocas de Carro">
             <Card>
-              <CardBody>
-                Trocas de Carro: Aceitamos seu carro usado como parte do
-                pagamento na compra de um veículo em nosso estoque. Avaliamos
-                seu carro de forma justa e transparente, garantindo uma
-                transição suave para o seu novo veículo.
-              </CardBody>
+              <CardBody>Trocas de Carro: {db[3]}</CardBody>
             </Card>
           </Tab>
         </Tabs>
